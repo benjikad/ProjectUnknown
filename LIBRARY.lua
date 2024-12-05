e02733413a99450be8415fd628bf37140f57abaa
benjikad <123773963+benjikad@users.noreply.github.com>
2024-12-05 16:50:31 -0500

    Update LIBRARY.lua

--- LIBRARY.lua
+++ LIBRARY.lua
@@ -1,2 +1,24 @@
-warn('Library is currently still in development.')
-return {}
+local lib = {}
+
+local themes = {
+    ['default'] = {
+        ['base'] = Color3.fromRGB(25,25,25),
+    },
+}
+
+function lib.new(theme)
+    theme = themes[theme or ''] or themes['default']
+
+    local g = Instance.new('ScreenGui')
+    g.Parent = game:GetService('CoreGui')
+
+    local f = Instance.new('Frame')
+    f.Size = UDim2.new(.25,0,.25,0)
+    f.BackgroundColor3 = theme.base
+
+    f.Parent = g
+
+    return f
+end
+
+return lib
