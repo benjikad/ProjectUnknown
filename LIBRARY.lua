259261900747d00bde87efd781865325a59d7a58
benjikad <123773963+benjikad@users.noreply.github.com>
2024-12-05 16:53:20 -0500

    Update LIBRARY.lua

--- LIBRARY.lua
+++ LIBRARY.lua
@@ -14,6 +14,7 @@ function lib.new(theme)
 
     local f = Instance.new('Frame')
     f.Size = UDim2.new(.25,0,.25,0)
+    f.Position = UDim2.new(.25,0,.25,0)
     f.BackgroundColor3 = theme.base
 
     f.Parent = g
